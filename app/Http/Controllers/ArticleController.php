549c4d3610e43f6fbafa389355026d8172e1d8f6
Htein Lynn Aung <hteinlynnaung.1111@gmail.com>
2026-09-21 23:55:08 +0700

    Update MyCode

---
+++ app/Http/Controllers/ArticleController.php
@@ -0,0 +1,34 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use App\Models\Article;
+
+class ArticleController extends Controller
+{
+    public function index()
+    { 
+        // Databaseကိုconnectလုပ်တာတွေ Query runတာတွေ menualလုပ်စရာမလိုဘူး
+        // ORM - Object Relational Mapping
+        // Database tableထဲက dataတွေကို objectပြောင်းပေးတဲ့နည်းပညာ
+
+        // Controllerက dataပေးမယ်  viewက UIလုပ်မယ်  Controllerမှာပဲ outputပြန်ထုတ်မယ်
+        $data = Article::all(); // Controllerက viewကို dataပေး
+
+        // Controllerကနေ views articlesထဲက index templateကို dataပေးပြီးလှမ်းခေါ်
+        // ("folder name.file name")
+        // Controllerမှာပဲ outputပြန်ထုတ်တယ်
+        return view("articles.index", [
+            'articles' => $data 
+        ]); 
+    } 
+
+    /**
+     * @param mixed $id
+     */
+    public function detail($id)
+    {
+        return "Article Controller Detail - $id";
+    }
+}
--- routes/web.php
+++ routes/web.php
@@ -1,17 +1,21 @@
 <?php
 
+use App\Http\Controllers\ArticleController;
 use Illuminate\Support\Facades\Route;
 
 // Static Route
-Route::get('/articles', function() {
-    return "Articles List";
-});
+Route::get('/articles', [ArticleController::class, 'index']);
+// localhost:8000/articles
 
 // Dynamic Route
-Route::get('/articles/detail/{id}', function($id) { // we can reuse {id} in return with $id
-    return "Articles Detail - $id";
-});
+Route::get('/articles/detail/{id}', [ArticleController::class, 'detail']);
+// localhost:8000/articles/detail/123
+
 
 Route::get('/', function () {
     return view('welcome');
 });
+
+Auth::routes();
+
+Route::get('/home', [App\Http\Controllers\HomeController::class, 'index'])->name('home');
