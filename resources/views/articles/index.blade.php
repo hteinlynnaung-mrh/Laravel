0f573a3b4d1a3333e484ced2ee43df8f6d3feedf
Htein Lynn Aung <hteinlynnaung.1111@gmail.com>
2026-09-22 22:26:56 +0700

    Update MyCode

--- resources/views/articles/index.blade.php
+++ resources/views/articles/index.blade.php
@@ -1,18 +1,25 @@
-<!DOCTYPE html>
-<html lang="en">
-<head>
-    <meta charset="UTF-8">
-    <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <meta http-equiv="X-UA-Compatible" content="ie=edge">
-    <title>Article List</title>
-</head>
-<body>
-    <h1>Article List</h1>
-    <ul> {{-- Controllerကပေးတဲ့ dataကို bladeနဲ့ယူတယ် --}}
-         {{-- bladeက php tapအဖွင့်အပိတ်တွေအစား @foreachသုံးတယ် outputထုတ်ရင် {{  }} --}}
-        @foreach ($articles as $article) 
-            <li>{{ $article['title'] }}</li>
+@extends("layouts.app")
+
+@section("content")
+    <div class="container" style="max-width: 800px">
+        @foreach($articles as $article)
+            <div class="card mb-2">
+                <div class="card-body">
+                    <h4>{{ $article->title }}</h4>
+                    <div class="text-muted">
+                        {{ $article->created_at->diffforHumans() }}
+                    </div>
+                    <div>
+                        {{ $article->body }}
+                    </div>
+                    <a href="{{ url("/articles/detail/$article->id") }}">
+                        View Detail
+                    </a>
+                </div>
+            </div>
         @endforeach
-    </ul>
-</body>
-</html>
+    </div>
+@endsection
+
+{{-- Controllerကပေးတဲ့ dataကို bladeနဲ့ယူတယ် --}}
+{{-- bladeက php tapအဖွင့်အပိတ်တွေအစား @foreachသုံးတယ် outputထုတ်ရင် {{  }} --}}
